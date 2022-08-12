19585271ee3ca4e5fbf90d56f033492228f701cf
Robert <robert_m@polbox.tv>
2022-08-12 14:28:57 +0300

    Fix and done.

--- TestWeather/Extensions/Array.swift
+++ TestWeather/Extensions/Array.swift
@@ -6,3 +6,14 @@
 //
 
 import Foundation
+
+extension Array where Element: Equatable {
+    var unique: [Element] {
+        var uniqueValues: [Element] = []
+        forEach { item in
+            guard !uniqueValues.contains(item) else { return }
+            uniqueValues.append(item)
+        }
+        return uniqueValues
+    }
+}
